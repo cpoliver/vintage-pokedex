6ac93cd2b830a6f9284c5c9d7a2951dfec534f0d
Charles P. Oliver <cpoliver@gmail.com>
2017-05-19 22:41:59 +0200

    Add header for pokemon detail

--- src/components/pokemon-detail.js
+++ src/components/pokemon-detail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 import Card from './card';
 import CardSection from './card-section';
@@ -7,9 +7,28 @@ import CardSection from './card-section';
 const PokemonDetail = ({ name }) => (
   <Card>
     <CardSection>
-      <Text>{name}</Text>
+      <View style={styles.headerContentStyle}>
+        <Image source={require('../../res/pokeball.png')} style={styles.iconStyle} />
+        <Text style={styles.titleStyle}>{name}</Text>
+      </View>
     </CardSection>
   </Card>
 );
+
+const styles = {
+  headerContentStyle: {
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  iconStyle: {
+    height: 24,
+    marginRight: 10,
+    width: 24
+  },
+  titleStyle: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+};
 
 export default PokemonDetail;
