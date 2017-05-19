f24316afe53a63848ece846848cd54f3a61a8344
Charles P. Oliver <cpoliver@gmail.com>
2017-05-19 20:26:55 +0200

    Enhance header component

--- index.ios.js
+++ index.ios.js
@@ -4,7 +4,7 @@ import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
 
 const App = () => (
-  <Header />
+  <Header text="Pokémon" />
 );
 
 AppRegistry.registerComponent('vintagePokedex', () => App);
--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
-const Header = () => (
-  <Text>Pokemon Name</Text>
-);
+const Header = ({ text }) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{text}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
+    elevation: 2,
+    height: 60,
+    justifyContent: 'center',
+    paddingTop: 15,
+    position: 'relative',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2
+  }
+};
 
 export default Header;
