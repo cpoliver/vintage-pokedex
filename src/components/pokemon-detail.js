ff46bedac6d425c417814343766b532ef9cdf21e
Charles P. Oliver <cpoliver@gmail.com>
2017-05-19 21:44:04 +0200

    Add card section component

--- src/components/pokemon-detail.js
+++ src/components/pokemon-detail.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Card from './card';
+import CardSection from './card-section';
 
 const PokemonDetail = ({ name }) => (
   <Card>
-    <Text>{name}</Text>
+    <CardSection>
+      <Text>{name}</Text>
+    </CardSection>
   </Card>
 );
 
