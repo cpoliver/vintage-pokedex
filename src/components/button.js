b077f16c66d9ca91496550316bb0b65a41396501
Charles P. Oliver <cpoliver@gmail.com>
2017-05-20 12:23:05 +0200

    Add button component

---
+++ src/components/button.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = ({ text, onPress }) => {
+  const { buttonStyle, textStyle } = styles;
+
+  return (
+    <TouchableOpacity style={buttonStyle} onPress={onPress}>
+      <Text style={textStyle}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = {
+  buttonStyle: {
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderColor: '#007aff',
+    borderRadius: 5,
+    borderWidth: 1,
+    flex: 1,
+    marginLeft: 5,
+    marginRight: 5
+  },
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingBottom: 10,
+    paddingTop: 10
+  }
+};
+
+export default Button;
