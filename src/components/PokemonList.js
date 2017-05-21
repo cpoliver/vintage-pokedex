9b9d463695822e3c0db02afeceda8576883ed3a7
Charles P. Oliver <cpoliver@gmail.com>
2017-05-21 13:20:33 +0200

    Show spinner when loading pokemon list

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,27 +2,39 @@ import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
 import axios from 'axios';
 
+import { Spinner } from './common';
 import PokemonDetail from './PokemonDetail';
 
 class PokemonList extends Component {
-  state = { pokemon: [] };
+  state = {
+    loading: true,
+    pokemon: []
+  };
 
   componentWillMount() {
     axios.get('https://api.myjson.com/bins/spn2d')
-      .then(response => this.setState({ pokemon: response.data }));
+         .then(response => this.setState({
+           loading: false,
+           pokemon: response.data
+         }));
   }
 
   renderPokemon() {
+    if (this.state.loading) {
+      return <Spinner />
+    }
+
     return this.state.pokemon.map(pokemon =>
       <PokemonDetail {...pokemon} key={pokemon.name} />
     );
   }
 
   render() {
-    const { pokemon } = this.state;
+    const { loading, pokemon } = this.state;
+    const style = loading ? { flex: 1 } : {};
 
     return (
-      <ScrollView>
+      <ScrollView contentContainerStyle={style}>
         {this.renderPokemon()}
       </ScrollView>
     );
