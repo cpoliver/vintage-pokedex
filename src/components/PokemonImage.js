e5801d3551d7c6f6011e16201a4402c2605cd62b
Charles P. Oliver <cpoliver@gmail.com>
2017-05-23 18:35:25 +0200

    Add loading spinner to pokemon image component

--- src/components/PokemonImage.js
+++ src/components/PokemonImage.js
@@ -1,16 +1,59 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { Component } from 'react';
+import { Image, View } from 'react-native';
 
-const PokemonImage = ({ image }) => (
-  <Image style={styles.imageStyle} source={{uri: image}} resizeMode="contain" />
-);
+import { Spinner } from './common';
+
+class PokemonImage extends Component {
+  state = { loading: true };
+
+  renderSpinner() {
+    const { overlayStyle } = styles;
+
+    return (
+      <View style={overlayStyle}>
+        <Spinner />
+      </View>
+    );
+  }
+
+  render() {
+    const { containerStyle, imageStyle } = styles;
+    const { image } = this.props;
+    const { loading } = this.state;
+
+    return (
+      <View style={containerStyle}>
+        <Image
+          style={imageStyle}
+          source={{uri: image}}
+          resizeMode="contain"
+          onLoad={() => this.setState({ loading: false })}
+          onLoadStart={() => this.setState({ loading: true })}
+        />
+        {loading && this.renderSpinner()}
+      </View>
+    );
+  }
+}
 
 const styles = {
+  containerStyle: {
+    flex: 1
+  },
   imageStyle: {
     flex: 1,
     height: 180,
-    margin: 40,
+    margin: 20,
     width: null
+  },
+  overlayStyle: {
+    backgroundColor: '#fff',
+    bottom: 0,
+    flex: 1,
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0
   }
 };
 
