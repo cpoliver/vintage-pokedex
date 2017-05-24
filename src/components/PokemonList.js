ff11a8c8176a4508f75007b6f3e1a0fe2fc065b4
Charles P. Oliver <cpoliver@gmail.com>
2017-05-24 19:25:28 +0200

    Remove unused axios lib

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
-import axios from 'axios';
 
 import { Spinner } from './common';
 import PokemonListItem from './PokemonListItem';
